e8dbc4b5f2481f5fc2c873490214e9f653ccf6d2
John Lakness <johnlakness@gmail.com>
2017-04-11 19:55:17 +0000

    trying for oculus support - grinch patch 031

--- UVCKernelPatches/scripts/applyUVCPatch.sh
+++ UVCKernelPatches/scripts/applyUVCPatch.sh
@@ -5,6 +5,6 @@ cd /usr/src/kernel
 echo $PATCHDIR
 patch -p 4 -i $PATCHDIR/patchUVCDriver.diff
 patch -p 4 -i $PATCHDIR/patchUVCInclude.diff
-
+patch -p 1 -i $PATCHDIR/031-patch-21.3-use_constrait_workaround.diff
 
 
